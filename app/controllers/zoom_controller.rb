75cdd72b5d48358b819b69ff70000ca0c25b13d0
kei178 <keisuke.inaba@hey.com>
2021-09-14 14:56:53 +0200

    Update zoom controller to use user me endpoint

--- app/controllers/zoom_controller.rb
+++ app/controllers/zoom_controller.rb
@@ -6,6 +6,16 @@ class ZoomController < ApplicationController
   end
 
   def auth
+    access_token = fetch_access_token
+    headers = { 'Authorization' => "Bearer #{access_token}" }
+    response = HTTParty.get('https://api.zoom.us/v2/users/me', headers: headers)
+    render json: { current_user: response.parsed_response }
+  end
+
+  private
+
+  # https://marketplace.zoom.us/docs/guides/auth/oauth#getting-access-token
+  def fetch_access_token
     url = 'https://zoom.us/oauth/token?grant_type=authorization_code&' +
           "code=#{params[:code]}&" + "redirect_uri=#{ENV['ZOOM_REDIRECT_URL']}"
     headers = {
@@ -13,10 +23,8 @@ class ZoomController < ApplicationController
       'Content-Type' => 'application/x-www-form-urlencoded'
     }
     response = HTTParty.post(url, headers: headers)
-    render json: response.parsed_response
+    response.parsed_response['access_token']
   end
-
-  private
 
   def encoded_client_credentials
     Base64.strict_encode64("#{ENV['ZOOM_CLIENT_ID']}:#{ENV['ZOOM_CLIENT_SECRET']}")
